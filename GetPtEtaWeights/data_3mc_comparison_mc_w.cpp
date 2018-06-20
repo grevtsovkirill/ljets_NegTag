894bcbac07b4978e97c4b570b3c0f411374d24d2
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2018-06-20 10:47:59 +0200

    comment sherpa, add input for compaigne and different legend depending on it

--- GetPtEtaWeights/data_3mc_comparison_mc_w.cpp
+++ GetPtEtaWeights/data_3mc_comparison_mc_w.cpp
@@ -1,7 +1,9 @@
 #include "../config/subTagger.hpp"
 #include "../AtlasStyle/AtlasStyle.C"
+#include <sys/stat.h>
+#include <sys/types.h>
 
-void data_3mc_comparison_mc_w(string dname="data_FlavourTagging_Nominal_0.root",string mname="mc_FlavourTagging_Nominal_Pythia.root",string mHname="mc_FlavourTagging_Nominal_Herwig.root",string mSname="mc_FlavourTagging_Nominal_Sherpa.root")
+void data_3mc_comparison_mc_w(string compaigne="a", string dname="data_FlavourTagging_Nominal_0.root",string mname="mc_FlavourTagging_Nominal_Pythia.root",string mHname="mc_FlavourTagging_Nominal_Herwig.root",string mSname="")
 {
   SetAtlasStyle();
 
@@ -9,9 +11,34 @@ void data_3mc_comparison_mc_w(string dname="data_FlavourTagging_Nominal_0.root",
   TFile *f_mc = new TFile(mname.c_str(), "read");
   //TFile *f_mc = new TFile("mc.root", "read");
   TFile *f_mc_HERWIG = new TFile(mHname.c_str(), "read");
-  TFile *f_mc_SHERPA = new TFile(mSname.c_str(), "read");
-  TFile *f_output = new TFile("Control_plots/control_plots.root", "recreate");
+  TFile *f_mc_SHERPA; 
+  if(!mSname.empty()){
+    f_mc_SHERPA= new TFile(mSname.c_str(), "read");
+  }
+  string outfolder="Control_plots_"+compaigne;
+  //Directory::CreateDirectory(outfolder.c_str(), NULL);
+  //Directory::CreateDirectory((outfolder+"/pdf").c_str(), NULL);
+  //_mkdir(outfolder.c_str());
 
+  cout << "compaigne "<< compaigne<< "; compaigne.find(a) = "<< compaigne.find("a")<<endl;
+  string text2="";
+  string text1="#sqrt{s} = 13 TeV, ";
+  if(compaigne.find("a") || compaigne.find("15") || compaigne.find("16")){
+    text2="36 fb^{-1} (2015+2016)";
+    cout << "found a, 15 or 16"<< endl; 
+  }
+  else if(compaigne.find("d") || compaigne.find("17") ){
+    cout << "             found d"<< endl; 
+    text2="46 fb^{-1} (2017)";
+  }
+  else text2="";
+  
+  text1+=text2;
+  
+  system(("mkdir "+outfolder).c_str());
+  system(("mkdir "+outfolder+"/pdf").c_str());
+  TFile *f_output = new TFile((outfolder+"/control_plots.root").c_str(), "recreate");
+  cout<< "sherpa input: "<<mSname<<endl;
   std::string suffix = "_mc_w";
   std::vector<std::string> histo_var = {"pt", "eta", "pt_thin", "eta_thin", 
                                         "phi", "njets_event", "averageInteractionsPerCrossing", 
@@ -85,9 +112,13 @@ void data_3mc_comparison_mc_w(string dname="data_FlavourTagging_Nominal_0.root",
       std::string histo_mc_HERWIG_name = "mc_HERWIG_" + histo_var.at(ivar) + jet_index + suffix; 
       TH1D *h_mc_HERWIG = (TH1D*)h_tmp->Clone(histo_mc_HERWIG_name.c_str());
 
-      h_tmp = (TH1D*)f_mc_SHERPA->Get(histo_name.c_str());
-      std::string histo_mc_SHERPA_name = "mc_SHERPA_" + histo_var.at(ivar) + jet_index + suffix; 
-      TH1D *h_mc_SHERPA = (TH1D*)h_tmp->Clone(histo_mc_SHERPA_name.c_str());
+      // TH1D *h_mc_SHERPA;
+      // if(!mSname.empty()){
+      // 	h_tmp = (TH1D*)f_mc_SHERPA->Get(histo_name.c_str());
+      // 	std::string histo_mc_SHERPA_name = "mc_SHERPA_" + histo_var.at(ivar) + jet_index + suffix; 
+      // 	h_mc_SHERPA = (TH1D*)h_tmp->Clone(histo_mc_SHERPA_name.c_str());
+      // }
+
       h_tmp =NULL;
 
       if(histo_var.at(ivar)=="jet_SV1_Lxy" ||
@@ -100,7 +131,7 @@ void data_3mc_comparison_mc_w(string dname="data_FlavourTagging_Nominal_0.root",
         h_data->Rebin(5);
         h_mc->Rebin(5);
         h_mc_HERWIG->Rebin(5);
-        h_mc_SHERPA->Rebin(5);
+	//	if(!mSname.empty()) h_mc_SHERPA->Rebin(5);
       }
  
       if(histo_var.at(ivar)=="jet_SV1_normdist" ||
@@ -109,7 +140,7 @@ void data_3mc_comparison_mc_w(string dname="data_FlavourTagging_Nominal_0.root",
         h_data->Rebin(25);
         h_mc->Rebin(25);
         h_mc_HERWIG->Rebin(25);
-        h_mc_SHERPA->Rebin(25);
+	//if(!mSname.empty()) h_mc_SHERPA->Rebin(25);
       }
  
       if(histo_var.at(ivar)=="jet_JetFitter_dRFlightDir" ||
@@ -118,7 +149,7 @@ void data_3mc_comparison_mc_w(string dname="data_FlavourTagging_Nominal_0.root",
         h_data->GetXaxis()->SetRangeUser(0,2);
         h_mc->GetXaxis()->SetRangeUser(0,2);
         h_mc_HERWIG->GetXaxis()->SetRangeUser(0,2);
-        h_mc_SHERPA->GetXaxis()->SetRangeUser(0,2);
+	//if(!mSname.empty()) h_mc_SHERPA->GetXaxis()->SetRangeUser(0,2);
       }
   
       if(histo_var.at(ivar)=="jet_SV1_NGTinSvx" ||
@@ -132,7 +163,7 @@ void data_3mc_comparison_mc_w(string dname="data_FlavourTagging_Nominal_0.root",
         h_data->GetXaxis()->SetRangeUser(0,30);
         h_mc->GetXaxis()->SetRangeUser(0,30);
         h_mc_HERWIG->GetXaxis()->SetRangeUser(0,30);
-        h_mc_SHERPA->GetXaxis()->SetRangeUser(0,30);
+	//if(!mSname.empty()) h_mc_SHERPA->GetXaxis()->SetRangeUser(0,30);
       }
   
       if(histo_var.at(ivar)=="jet_JetFitter_nSingleTracks" ||
@@ -143,7 +174,7 @@ void data_3mc_comparison_mc_w(string dname="data_FlavourTagging_Nominal_0.root",
         h_data->GetXaxis()->SetRangeUser(0,10);
         h_mc->GetXaxis()->SetRangeUser(0,10);
         h_mc_HERWIG->GetXaxis()->SetRangeUser(0,10);
-        h_mc_SHERPA->GetXaxis()->SetRangeUser(0,10);
+	//if(!mSname.empty()) h_mc_SHERPA->GetXaxis()->SetRangeUser(0,10);
       }
   
       if(histo_var.at(ivar)=="jet_SV1_Lxy" ||
@@ -156,7 +187,7 @@ void data_3mc_comparison_mc_w(string dname="data_FlavourTagging_Nominal_0.root",
         h_data->GetXaxis()->SetRangeUser(0,200);
         h_mc->GetXaxis()->SetRangeUser(0,200);
         h_mc_HERWIG->GetXaxis()->SetRangeUser(0,200);
-        h_mc_SHERPA->GetXaxis()->SetRangeUser(0,200);
+        //h_mc_SHERPA->GetXaxis()->SetRangeUser(0,200);
       }
 
       if(flag_logy)
@@ -164,29 +195,30 @@ void data_3mc_comparison_mc_w(string dname="data_FlavourTagging_Nominal_0.root",
         h_data->SetMaximum(h_data->GetMaximum()*10000);
         h_mc->SetMaximum(h_mc->GetMaximum()*10000);
         h_mc_HERWIG->SetMaximum(h_mc_HERWIG->GetMaximum()*10000);
-        h_mc_SHERPA->SetMaximum(h_mc_SHERPA->GetMaximum()*10000);
+	///if(!mSname.empty())         h_mc_SHERPA->SetMaximum(h_mc_SHERPA->GetMaximum()*10000);
       }
       if(!flag_logy)
       {
         h_data->SetMaximum(h_data->GetMaximum()*1.3);
         h_mc->SetMaximum(h_mc->GetMaximum()*1.3);
         h_mc_HERWIG->SetMaximum(h_mc_HERWIG->GetMaximum()*1.3);
-        h_mc_SHERPA->SetMaximum(h_mc_SHERPA->GetMaximum()*1.3);
+	//if(!mSname.empty()) h_mc_SHERPA->SetMaximum(h_mc_SHERPA->GetMaximum()*1.3);
       }
 
       std::string histo_ratio_name = "ratio_" + histo_var.at(ivar) + jet_index + suffix; 
       std::string histo_ratio_HERWIG_name = "ratio_HERWIG_" + histo_var.at(ivar) + jet_index + suffix; 
-      std::string histo_ratio_SHERPA_name = "ratio_SHERPA_" + histo_var.at(ivar) + jet_index + suffix; 
+      //std::string histo_ratio_SHERPA_name = "ratio_SHERPA_" + histo_var.at(ivar) + jet_index + suffix; 
       TH1D *h_ratio = (TH1D*)h_data->Clone(histo_ratio_name.c_str());
       TH1D *h_ratio_HERWIG = (TH1D*)h_data->Clone(histo_ratio_HERWIG_name.c_str());
-      TH1D *h_ratio_SHERPA = (TH1D*)h_data->Clone(histo_ratio_SHERPA_name.c_str());
+      TH1D *h_ratio_SHERPA;
+      // if(!mSname.empty()) h_ratio_SHERPA = (TH1D*)h_data->Clone(histo_ratio_SHERPA_name.c_str());
       h_ratio->Divide(h_mc); 
       h_ratio_HERWIG->Divide(h_mc_HERWIG); 
-      h_ratio_SHERPA->Divide(h_mc_SHERPA); 
+      //if(!mSname.empty()) h_ratio_SHERPA->Divide(h_mc_SHERPA); 
 
       h_ratio->SetMaximum(5);
       h_ratio_HERWIG->SetMaximum(5);
-      h_ratio_SHERPA->SetMaximum(5);
+      //if(!mSname.empty()) h_ratio_SHERPA->SetMaximum(5);
       h_ratio->SetMinimum(0);
       h_ratio->SetMinimum(0);
 
@@ -215,17 +247,19 @@ void data_3mc_comparison_mc_w(string dname="data_FlavourTagging_Nominal_0.root",
       h_mc_HERWIG->GetYaxis()->SetTitleOffset(1.30);
       h_mc_HERWIG->GetYaxis()->SetNdivisions(505);
 
-      h_mc_SHERPA->SetLineColor(kBlue-3);
-      h_mc_SHERPA->SetMarkerColor(kBlue-3);
-      h_mc_SHERPA->SetXTitle(histo_Xlabels.at(ivar).c_str());
-      h_mc_SHERPA->SetYTitle("Events");
-      h_mc_SHERPA->SetTitle("");
-      h_mc_SHERPA->GetXaxis()->SetLabelSize(0);
-      h_mc_SHERPA->GetXaxis()->SetTitleSize(0);
-      h_mc_SHERPA->GetYaxis()->SetLabelSize(0.06);
-      h_mc_SHERPA->GetYaxis()->SetTitleSize(0.06);
-      h_mc_SHERPA->GetYaxis()->SetTitleOffset(1.30);
-      h_mc_SHERPA->GetYaxis()->SetNdivisions(505);
+      // if(!mSname.empty()){
+      // h_mc_SHERPA->SetLineColor(kBlue-3);
+      // h_mc_SHERPA->SetMarkerColor(kBlue-3);
+      // h_mc_SHERPA->SetXTitle(histo_Xlabels.at(ivar).c_str());
+      // h_mc_SHERPA->SetYTitle("Events");
+      // h_mc_SHERPA->SetTitle("");
+      // h_mc_SHERPA->GetXaxis()->SetLabelSize(0);
+      // h_mc_SHERPA->GetXaxis()->SetTitleSize(0);
+      // h_mc_SHERPA->GetYaxis()->SetLabelSize(0.06);
+      // h_mc_SHERPA->GetYaxis()->SetTitleSize(0.06);
+      // h_mc_SHERPA->GetYaxis()->SetTitleOffset(1.30);
+      // h_mc_SHERPA->GetYaxis()->SetNdivisions(505);
+      // }
 
       h_ratio->SetXTitle(histo_Xlabels.at(ivar).c_str());
       h_ratio->SetYTitle("Data / MC");
@@ -249,16 +283,18 @@ void data_3mc_comparison_mc_w(string dname="data_FlavourTagging_Nominal_0.root",
       h_ratio_HERWIG->GetXaxis()->SetTitleSize(0.12);       
       h_ratio_HERWIG->GetXaxis()->SetTitleOffset(1.36);       
 
-      h_ratio_SHERPA->SetXTitle(histo_Xlabels.at(ivar).c_str());
-      h_ratio_SHERPA->SetYTitle("Data / MC");
-      h_ratio_SHERPA->SetTitle("");
-      h_ratio_SHERPA->GetYaxis()->SetLabelSize(0.12);
-      h_ratio_SHERPA->GetYaxis()->SetTitleSize(0.12);
-      h_ratio_SHERPA->GetYaxis()->SetTitleOffset(0.60);
-      h_ratio_SHERPA->GetYaxis()->SetNdivisions(505);
-      h_ratio_SHERPA->GetXaxis()->SetLabelSize(0.12);
-      h_ratio_SHERPA->GetXaxis()->SetTitleSize(0.12);       
-      h_ratio_SHERPA->GetXaxis()->SetTitleOffset(1.36);       
+      // if(!mSname.empty()){
+      // h_ratio_SHERPA->SetXTitle(histo_Xlabels.at(ivar).c_str());
+      // h_ratio_SHERPA->SetYTitle("Data / MC");
+      // h_ratio_SHERPA->SetTitle("");
+      // h_ratio_SHERPA->GetYaxis()->SetLabelSize(0.12);
+      // h_ratio_SHERPA->GetYaxis()->SetTitleSize(0.12);
+      // h_ratio_SHERPA->GetYaxis()->SetTitleOffset(0.60);
+      // h_ratio_SHERPA->GetYaxis()->SetNdivisions(505);
+      // h_ratio_SHERPA->GetXaxis()->SetLabelSize(0.12);
+      // h_ratio_SHERPA->GetXaxis()->SetTitleSize(0.12);       
+      // h_ratio_SHERPA->GetXaxis()->SetTitleOffset(1.36);       
+      // }
 
       TLegend* legend = new TLegend(0.580201,0.561807,0.780702,0.706947);
       legend->SetTextFont(42);
@@ -277,14 +313,15 @@ void data_3mc_comparison_mc_w(string dname="data_FlavourTagging_Nominal_0.root",
       legend_HERWIG->AddEntry(h_data,"Data","LPE");
       legend_HERWIG->AddEntry(h_mc_HERWIG,"HERWIG MC","LPE");
 
-      TLegend* legend_SHERPA = new TLegend(0.580201,0.561807,0.780702,0.706947);
-      legend_SHERPA->SetTextFont(42);
-      legend_SHERPA->SetBorderSize(0);  // no border
-      legend_SHERPA->SetFillColor(0);   // Legend background should be white
-      legend_SHERPA->SetTextSize(0.04); // Increase entry font size!
-      legend_SHERPA->AddEntry(h_data,"Data","LPE");
-      legend_SHERPA->AddEntry(h_mc_SHERPA,"SHERPA","LPE");
-
+      // TLegend* legend_SHERPA = new TLegend(0.580201,0.561807,0.780702,0.706947);
+      // legend_SHERPA->SetTextFont(42);
+      // legend_SHERPA->SetBorderSize(0);  // no border
+      // legend_SHERPA->SetFillColor(0);   // Legend background should be white
+      // legend_SHERPA->SetTextSize(0.04); // Increase entry font size!
+      // if(!mSname.empty()){
+      // legend_SHERPA->AddEntry(h_data,"Data","LPE");
+      // legend_SHERPA->AddEntry(h_mc_SHERPA,"SHERPA","LPE");
+      // }
 
       TPaveText *pt = new TPaveText(0.508772,0.851259,0.629073,0.919908,"brNDC");
       pt->SetBorderSize(0);
@@ -305,7 +342,7 @@ void data_3mc_comparison_mc_w(string dname="data_FlavourTagging_Nominal_0.root",
       pt3->SetFillColor(0);
       pt3->SetTextSize(0.04);
       pt3->SetTextFont(42);
-      pt3->AddText("#sqrt{s} = 13 TeV, 3.2 + 32.9 fb^{-1}, rel21");
+      pt3->AddText(text1.c_str());
       if(ijet==2) pt3->AddText("Subleading jet");
       //pt3->AddText("p_{T}^{jet}/|#eta|^{jet} and p_{T}^{jet}/N_{trk}^{jet} reweighting applied");
 
@@ -366,41 +403,46 @@ void data_3mc_comparison_mc_w(string dname="data_FlavourTagging_Nominal_0.root",
       h_ratio_HERWIG->Draw("LPE");
       h_ratio_HERWIG->Draw("AXISSAME");
 
-      std::string canvas_SHERPA_name = "c_SHERPA_" + histo_var.at(ivar) + jet_index + suffix; 
-      std::string pad1_SHERPA_name = "pad1_SHERPA_" + histo_var.at(ivar) + jet_index + suffix; 
-      std::string pad2_SHERPA_name = "pad2_SHERPA_" + histo_var.at(ivar) + jet_index + suffix; 
-      TCanvas *c_SHERPA = new TCanvas(canvas_SHERPA_name.c_str(), "", 800, 900);
-      TPad* pad1_SHERPA = new TPad(pad1_SHERPA_name.c_str(),"", 0, 0.3, 1, 1.0);
-      pad1_SHERPA->SetBottomMargin(0.1);
-      TPad* pad2_SHERPA = new TPad(pad2_SHERPA_name.c_str(),"", 0, 0.01, 1, 0.35);
-      pad2_SHERPA->SetBottomMargin(0.35);
-      pad1_SHERPA->Draw();
-      pad2_SHERPA->Draw();
+      // TCanvas *c_SHERPA;
+      // std::string canvas_SHERPA_name = "c_SHERPA_" + histo_var.at(ivar) + jet_index + suffix; 
+      // if(!mSname.empty()){
+      //  std::string pad1_SHERPA_name = "pad1_SHERPA_" + histo_var.at(ivar) + jet_index + suffix; 
+      // std::string pad2_SHERPA_name = "pad2_SHERPA_" + histo_var.at(ivar) + jet_index + suffix; 
+      // c_SHERPA = new TCanvas(canvas_SHERPA_name.c_str(), "", 800, 900);
+      // TPad* pad1_SHERPA = new TPad(pad1_SHERPA_name.c_str(),"", 0, 0.3, 1, 1.0);
+      // pad1_SHERPA->SetBottomMargin(0.1);
+      // TPad* pad2_SHERPA = new TPad(pad2_SHERPA_name.c_str(),"", 0, 0.01, 1, 0.35);
+      // pad2_SHERPA->SetBottomMargin(0.35);
+      // pad1_SHERPA->Draw();
+      // pad2_SHERPA->Draw();
 
-      pad1_SHERPA->cd();
-      if(flag_logy) pad1_SHERPA->SetLogy();
-      if(flag_logx) pad1_SHERPA->SetLogx();
+      // pad1_SHERPA->cd();
+      // if(flag_logy) pad1_SHERPA->SetLogy();
+      // if(flag_logx) pad1_SHERPA->SetLogx();
 
-      h_mc_SHERPA->Draw("LPE");
-      h_data->Draw("LPESAME");
-      h_data->Draw("AXISSAME");
-      legend_SHERPA->Draw();
-      pt->Draw();
-      pt2->Draw();
-      pt3->Draw();
+      // h_mc_SHERPA->Draw("LPE");
+      // h_data->Draw("LPESAME");
+      // h_data->Draw("AXISSAME");
+      // legend_SHERPA->Draw();
+      // pt->Draw();
+      // pt2->Draw();
+      // pt3->Draw();
 
-      pad2_SHERPA->cd();
-      if(flag_logx) pad2_SHERPA->SetLogx();
-      h_ratio_SHERPA->Draw("LPE");
-      h_ratio_SHERPA->Draw("AXISSAME");
+      // pad2_SHERPA->cd();
+      // if(flag_logx) pad2_SHERPA->SetLogx();
+      // h_ratio_SHERPA->Draw("LPE");
+      // h_ratio_SHERPA->Draw("AXISSAME");
+      // }
 
       f_output->cd();
       c->Write();
-      c->SaveAs(("Control_plots/pdf/"+canvas_name+".pdf").c_str());
+      c->SaveAs((outfolder+"/pdf/"+canvas_name+".pdf").c_str());
       c_HERWIG->Write();
-      c->SaveAs(("Control_plots/pdf/"+canvas_HERWIG_name+".pdf").c_str());
-      c_SHERPA->Write();
-      c->SaveAs(("Control_plots/pdf/"+canvas_SHERPA_name+".pdf").c_str());
+      c->SaveAs((outfolder+"/pdf/"+canvas_HERWIG_name+".pdf").c_str());
+      // if(!mSname.empty()){
+      // c_SHERPA->Write();
+      // c->SaveAs((outfolder+"/pdf/"+canvas_SHERPA_name+".pdf").c_str());
+      // }
     }
 
     std::cout << "OK" << std::endl ;
