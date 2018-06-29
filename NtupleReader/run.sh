45b65cbb518a0a513dfa4bee1a033184a928853d
Kirill Grevtsov <kirill.grevtsov@cern.ch>
2018-06-29 14:02:31 +0200

    upd run options

--- NtupleReader/run.sh
+++ NtupleReader/run.sh
@@ -10,19 +10,19 @@ source ../helpers/run_mode.sh
 
 # Read ../config/systematics.txt line by line
 while read mode name type var; do
-
+    
     echo systematic: $name, mode: $mode
     # don't do anything for nomode, bootstrap and rewmode
     if [[ "$mode" == "nomode" ]] || [[ "$mode" == "bootstrap" ]] || [[ "$mode" == "rwmode" ]]; then
 	continue
     fi
-
+    
     # create directory for the syst.
     if [ ! -e res/$name ]; then
 	echo creating res/$name
 	mkdir res/$name
     fi
-
+    
     LOGNAME="$mode$name"
     # for "dataperiod" only - run on data only period one by one
     if [ $name == "dataperiod" ]; then
@@ -43,27 +43,27 @@ while read mode name type var; do
     elif [[ ${mode:0:6} == "doJets" ]] || [[ $mode == "xAOD2" ]] || [[ $mode == "xAOD" ]] && [[ $name != "FlavourTagging_Nominal" ]]; then
         # HERWIG
         if [[ $name == *"generator"* ]]; then
-          echo ./NtupleReaderApp -s $name -m $mode -f $(get_mcHERWIG_ntupledumper)
-          run ./NtupleReaderApp -s $name -m $mode -f $(get_mcHERWIG_ntupledumper)
+           #echo ./NtupleReaderApp -c "a" -s $name -m $mode -f $(get_mcHERWIG_ntupledumper)
+            run ./NtupleReaderApp -c "a" -s $name -m $mode -f $(get_mcHERWIG_ntupledumper)
         else 
-	  echo ./NtupleReaderApp -s $name -m $mode -f $(get_mc_ntupledumper)
-	  run ./NtupleReaderApp -s $name -m $mode -f $(get_mc_ntupledumper)
+	    #echo ./NtupleReaderApp -c "a" -s $name -m $mode -f $(get_mc_ntupledumper d)
+	    run ./NtupleReaderApp -c "a" -s $name -m $mode -f $(get_mc_ntupledumper d) 
         fi
         # use data from FlavourTagging_Nominal except if it contains "subleadingjet"
  	rm -f res/$name/data.root
         if [[ $name == *"subleadingjet"* ]]; then
-          ln -s ../subleadingjet/data.root res/$name/data.root
+            ln -s ../subleadingjet/data.root res/$name/data.root
         else 
-	  ln -s ../FlavourTagging_Nominal/data.root res/$name/data.root
+	    ln -s ../FlavourTagging_Nominal/data.root res/$name/data.root
         fi
     # remaining systematic (doSubLeadJet) and FlavourTagging_Nominal: run data and MC
     else
-	echo ./NtupleReaderApp -s $name -m $mode -f $(get_mc_ntupledumper)
-	run ./NtupleReaderApp -s $name -m $mode -f $(get_mc_ntupledumper)
+	echo ./NtupleReaderApp -c "a" -s $name -m $mode -f $(get_mc_ntupledumper $name)
+	run ./NtupleReaderApp -c "a" -s $name -m $mode 
 	LOGNAME+="data"
-	echo ./NtupleReaderApp -d -s $name -m $mode -f $(get_data_ntupledumper)
-	run ./NtupleReaderApp -d -s $name -m $mode -f $(get_data_ntupledumper)
-  fi
+	echo ./NtupleReaderApp -c "a" -d -s $name -m $mode -f $(get_data_ntupledumper)
+	run ./NtupleReaderApp -c "a" -d -s $name -m $mode -f $(get_data_ntupledumper)
+    fi
 
 done < <(getCleanSysts)
 
